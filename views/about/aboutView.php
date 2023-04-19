cbd87df221f5d6071c5c4960bfc9ee3583f469e6
OleksandrOleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-04-19 22:53:24 +0200

    add contents about

--- views/about/aboutView.php
+++ views/about/aboutView.php
@@ -1,23 +1,107 @@
 <div class="text-emerald-900">
     <?= do_shortcode("[views section=general name=navbarView]"); ?>
-    <div id="aboutMe" class="w-100 min-h-screen">
-        <header class="py-10 lg:py-20 w-11/12 mx-auto">
-            <div class="flex flex-col sm:flex-row justify-center items-center pb-10 gap-10 lg:gap-20 ">
-                <div class="w-1/2 flex flex-col gap-2 max-w-2xl">
-                    <div class="text-left">
-                        <h2 class="text-3xl lg:text-5xl xl:text-6xl font-semibold mb-1 md:mb-3">
-                            ciao a tutti,
-                        </h2>
-                        <h1 class="text-4xl lg:text-6xl xl:text-7xl font-semibold mb-1 md:mb-3">sono Tania</h1>
-                        <p class="text-lg md:text-xl lg:text-2xl xl:text-3xl text-emerald-600">una sarta professionista con una passione per il cucito che risale alla mia infanzia. </p>
+    <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+        <!-- <div class="absolute inset-0 -z-10 overflow-hidden">
+            <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+                <defs>
+                    <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+                        <path d="M100 200V.5M.5 .5H200" fill="none" />
+                    </pattern>
+                </defs>
+                <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+                    <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+                </svg>
+                <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+            </svg>
+        </div> -->
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+            <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                <div class="lg:pr-4">
+                    <div class="lg:max-w-lg">
+                        <p class="text-base font-semibold leading-7 text-pink-500">Mi presento</p>
+                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-emerald-700 sm:text-4xl">Mi chiamo Tania</h1>
+                        <p class="mt-6 text-xl leading-8 text-gray-700">Sono appassionata di moda e ho deciso di fare di questa passione il mio mestiere fin da quando avevo 7 o 8 anni, quando ho iniziato a cucire. Con il tempo, il cucito è diventato sempre più importante per me e ho sviluppato una profonda passione per questo lavoro.</p>
                     </div>
                 </div>
-                <!-- image -->
-                <div class="w-1/2 max-w-xs lg:max-w-sm  shadow-lg shadow-pink-400">
-                    <img class="" src="<?= get_image_path("foto.jpeg"); ?>" alt="Header Image">
+            </div>
+            <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                <img class="w-[48rem] max-w-2xl rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="<?= get_image_path('foto.jpeg'); ?>" alt="">
+            </div>
+            <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                <div class="lg:pr-4">
+                    <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+                        <p>Ho completato una formazione di 5 anni presso una scuola specializzata, che mi ha permesso di acquisire competenze tecniche e di sviluppare la mia creatività.</p>
+                        <ul role="list" class="mt-8 space-y-8 text-gray-600">
+                            <li class="flex gap-x-3">
+                                <svg fill="#f43f5e" height="64px" width="64px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <g id="Books_1_">
+                                            <path d="M62.9994125,39.4741516h-1.9568176c-2.3530006-5.0440025-2.3532982-9.9673023,0-15.0459023h1.9568176 c0.5526848,0,1-0.4477997,1-1c0-0.5522995-0.4473152-1-1-1H25.8977966v-0.7432003h31.9053001c0.5527,0,1-0.4476986,1-1 c0-0.5521984-0.4473-1-1-1h-1.9568024c-2.3532982-5.0436993-2.3522987-9.9664993,0.0003014-15.0463991h1.956501 c0.5527,0,1-0.4477,1-1c0-0.5521998-0.4473-1-1-1H5.2444973c-0.3632998,0-0.6981997,0.1973-0.875,0.5152001 C0.7366974,9.70895,0.7522975,15.7949495,4.414412,21.2431507c0.1855855,0.2763996,0.4970856,0.4418983,0.8300853,0.4418983 h10.1299143v0.7432003h-4.9336147c-0.3632994,0-0.6981993,0.1972008-0.875,0.5151005 C5.9319973,29.4980507,5.9475975,35.58395,9.6106968,41.0322495c0.1856003,0.2763023,0.4971008,0.4419022,0.8301001,0.4419022 h52.5586166c0.5526848,0,1-0.4478035,1-1C63.9994125,39.9218483,63.5520973,39.4741516,62.9994125,39.4741516z M17.3744125,11.6435499h6.5233841v17.3358994l-2.8486004-1.2915001c-0.2636986-0.1191006-0.5625-0.1191006-0.8261986,0 l-2.8485851,1.2915001V11.6435499z M5.7874975,19.6850491c-2.875-4.5765991-2.8574002-9.5087986,0.0537-15.0463991h47.8217964 c-2.0849991,5.0116997-2.0861969,10.0642004-0.0029984,15.0463991H25.8977966v-8.0414991h16.0458984c0.552803,0,1-0.4476995,1-1 c0-0.5522003-0.447197-1-1-1H12.1126976c-0.5528002,0-1,0.4477997-1,1c0,0.5523005,0.4471998,1,1,1h3.261714v8.0414991H5.7874975z M11.0374975,24.4282494h4.3369141v6.1025009c0,0.3398991,0.1727858,0.6562996,0.4580002,0.8402996 c0.2860861,0.1846008,0.6464853,0.2115002,0.9550848,0.0704002l3.8486004-1.7446995l3.8486004,1.7446995 c0.1318989,0.0599995,0.2724991,0.0893002,0.4130993,0.0893002c0.1895008,0,0.3780003-0.0536995,0.5419998-0.1597004 c0.2852001-0.184,0.4580002-0.5004005,0.4580002-0.8402996v-6.1025009h32.9612007 c-2.085701,5.0107002-2.0862007,10.0629997-0.0023994,15.0459023H10.9827976 C8.1077976,34.8974495,8.125412,29.9658508,11.0374975,24.4282494z"></path>
+                                            <path d="M54.1919975,44.3154488h1.9567986c0.5527,0,1-0.4477997,1-1c0-0.5522995-0.4473-1-1-1H3.5901973 c-0.3632998,0-0.6982,0.1972008-0.875,0.5151024c-3.6346998,6.5546989-3.6190999,12.6405983,0.0449002,18.0888977 c0.1866,0.2764015,0.4970999,0.4419022,0.8300998,0.4419022h52.5585976c0.5527,0,1-0.4477997,1-1c0-0.5522995-0.4473-1-1-1 h-1.9567986C51.8389969,54.3168488,51.8387947,49.3940506,54.1919975,44.3154488z M4.1321974,59.361351 c-2.8759999-4.5767021-2.8583999-9.5088005,0.0546999-15.0459023h47.8215141 c-2.0857162,5.0107002-2.0859146,10.0627022-0.0024147,15.0459023H4.1321974z"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                                <span><strong class="font-semibold text-gray-900">Studio.</strong> Sono sempre alla ricerca di nuove sfide e cerco di imparare continuamente nuove tecniche di cucito per creare capi unici e originali. </span>
+                            </li>
+                            <li class="flex gap-x-3">
+                                <svg fill="#f43f5e" width="64px" height="64px" viewBox="0 0 64 64" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <title></title>
+                                        <path d="M2,15.01978a9.76365,9.76365,0,0,0,.7074,2.10382l2.02087.18493a8.35412,8.35412,0,0,0,.8385,1.13373L5.1239,20.376a10.21966,10.21966,0,0,0,1.83179,1.311L8.69281,20.68a8.58722,8.58722,0,0,0,1.36877.42761L10.8681,23h2.26374l.80645-1.89233a8.58966,8.58966,0,0,0,1.36872-.42762l1.73718,1.007,1.83173-1.31091L18.433,18.44244a8.34469,8.34469,0,0,0,.83863-1.13372l2.02093-.18494A9.76526,9.76526,0,0,0,22,15.0199L20.47095,13.702V12.298L22,10.98016a9.76351,9.76351,0,0,0-.70734-2.10382l-2.02093-.18493a8.35724,8.35724,0,0,0-.8385-1.13373L18.8761,5.624a10.22051,10.22051,0,0,0-1.83179-1.311L15.30719,5.32a8.574,8.574,0,0,0-1.36877-.42761L13.1319,3H10.86816l-.80645,1.89233A8.58443,8.58443,0,0,0,8.693,5.32l-1.73718-1.007A10.21827,10.21827,0,0,0,5.12408,5.62384L5.567,7.55756a8.33923,8.33923,0,0,0-.83857,1.13372l-2.02093.18494A9.75883,9.75883,0,0,0,2,10.9801l1.529,1.31781V13.702ZM12,9a4,4,0,1,1-4,4A4,4,0,0,1,12,9Z"></path>
+                                        <rect height="8" width="8" x="5" y="54"></rect>
+                                        <rect height="11" width="8" x="20" y="51"></rect>
+                                        <rect height="15" width="8" x="35" y="47"></rect>
+                                        <rect height="24" width="8" x="50" y="38"></rect>
+                                        <path d="M36.87891,24.52344a1.001,1.001,0,0,0-.604-.48535l-7-2-.5498,1.92382,4.90051,1.40015L3.45117,45.16406l1.09766,1.67188L34.22809,27.35889,32.583,32.70605l1.91113.5879,2.46143-8A.99753.99753,0,0,0,36.87891,24.52344Z"></path>
+                                        <polygon points="45.5 15.935 44.674 21 49 18.609 53.326 21 52.5 15.935 56 12.348 51.163 11.609 49 7 46.837 11.609 42 12.348 45.5 15.935"></polygon>
+                                        <path d="M50.91992,25.81818a10.9111,10.9111,0,0,1-1.90478.16717H49a10.94684,10.94684,0,0,1-1.89941-.16522l-.34375,1.98511A19.795,19.795,0,0,0,49.01611,28a12.86532,12.86532,0,0,0,2.25147-.19873Z"></path>
+                                        <path d="M54.50488,24.499l1.00293,1.74311a13.0404,13.0404,0,0,0,3.45313-2.92255l-1.53125-1.29651A11.05688,11.05688,0,0,1,54.50488,24.499Z"></path>
+                                        <path d="M57.40283,7.7536a11.05242,11.05242,0,0,1,1.92237,3.322l1.877-.69647a13.049,13.049,0,0,0-2.273-3.9259Z"></path>
+                                        <path d="M46.70605,2.01178a12.85586,12.85586,0,0,0-4.2373,1.568l1.00684,1.74115a10.8707,10.8707,0,0,1,3.583-1.326Z"></path>
+                                        <path d="M61.22021,19.38379a13.18675,13.18675,0,0,0,.7793-4.50824l-.4414-.00787L60,14.905a11.12449,11.12449,0,0,1-.65967,3.79022Z"></path>
+                                        <path d="M55.46387,3.54144A12.8159,12.8159,0,0,0,51.2168,2l-.33887,1.98511a10.8563,10.8563,0,0,1,3.59082,1.30334Z"></path>
+                                        <path d="M36.78906,19.40741,38.667,18.7149a11.10956,11.10956,0,0,1-.6665-3.78723l-2,.004A13.12678,13.12678,0,0,0,36.78906,19.40741Z"></path>
+                                        <path d="M40.583,22.03882l-1.5293,1.29846a13.04426,13.04426,0,0,0,3.4585,2.9176l1-1.745A11.03952,11.03952,0,0,1,40.583,22.03882Z"></path>
+                                        <path d="M36.77148,10.4519l1.88086.68464a11.05087,11.05087,0,0,1,1.9043-3.33374L39.02148,6.51019A13.07675,13.07675,0,0,0,36.77148,10.4519Z"></path>
+                                    </g>
+                                </svg>
+                                <span><strong class="font-semibold text-gray-900">Sfida.</strong> Sfido me stessa quotidianamente, continuando a imparare nuove tecniche di cucito che mi aiutano a creare capi unici e originali.</span>
+                            </li>
+                            <li class="flex gap-x-3">
+                                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 285.334 285.334" xml:space="preserve" width="64px" height="64px" fill="#000000">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <g>
+                                            <g>
+                                                <rect x="166.686" y="6.537" transform="matrix(0.5547 -0.8321 0.8321 0.5547 62.2534 163.1956)" style="fill:#f43f5e;" width="33.804" height="33.804"></rect>
+                                                <path style="fill:#f43f5e;" d="M168.385,238.191c1.847-0.67,3.563-1.702,5.016-3.127c3.169-3.113,4.65-7.42,4.064-11.827 l-7.931-59.48v-42.258l34.589-12.352c4.341-1.552,7.552-5.33,8.377-9.867l5.259-28.936l43.008,27.366l5.034-7.912l-46.528-29.606 c-0.108-3.033-1.205-5.972-3.192-8.353c-2.63-3.15-6.492-4.959-10.598-4.959c-1.913,0-3.717,0.417-5.377,1.111l-37.43-23.817 l-5.034,7.913l35.466,22.566c-0.591,1.223-1.05,2.527-1.27,3.933l-4.35,28.27l-27.516,9.314 c-5.133-4.547-11.742-7.102-18.563-7.102c-6.038,0-11.845,1.959-16.973,5.803l-1.448,1.2c-0.352,0.305-8.03,7.28-14.391,13.059 V94.257c0-7.106-5.044-13.284-11.733-14.372c-4.12-0.68-8.288,0.483-11.433,3.159c-3.155,2.677-4.959,6.581-4.959,10.711v38.217 c0,9.942,8.091,18.033,18.033,18.033c4.467,0,8.752-1.645,12.061-4.631l2.719-2.442v20.827l-7.884,59.137 c-0.9,6.75,2.963,13.111,9.042,15.291c-7.477,2.109-14.756,5.072-21.642,8.977l-52.964,30.014l4.622,8.156l52.964-30.014 c27.136-15.37,60.849-15.375,87.98,0l52.964,30.014l4.622-8.156l-52.964-30.014C183.136,243.263,175.857,240.301,168.385,238.191z M104.292,138.408c-1.589,1.434-3.647,2.222-5.789,2.222c-4.772,0-8.658-3.886-8.658-8.658V93.755c0-1.378,0.6-2.681,1.655-3.572 c1.069-0.905,2.433-1.27,3.858-1.05c2.166,0.356,3.863,2.606,3.863,5.123v36.061l7.838-7.125c0,0,21.281-19.341,21.994-19.973 l1.177-0.98c7.716-5.775,18.652-4.613,25.027,2.442l2.062,2.283l38.564-13.055l5.213-33.9c0.014-0.08,0.061-0.141,0.075-0.22 l8.044,5.119l-5.944,32.695c-0.23,1.247-1.111,2.287-2.306,2.714l-40.807,14.579v49.172l8.011,60.408 c0.192,1.453-0.295,2.873-1.341,3.9c-0.609,0.605-1.945,1.58-3.994,1.256c-1.936-0.295-3.553-2.119-3.848-4.341l-8.789-65.911 h-17.583l-8.841,66.314c-0.178,1.322-0.914,2.498-2.03,3.234c-1.13,0.75-2.512,0.952-3.895,0.577 c-2.095-0.581-3.488-2.934-3.164-5.367l7.927-59.452l0.047-42.806L104.292,138.408z M128.761,235.229 c2.32-2.189,3.881-5.086,4.308-8.292l7.753-58.181h1.167l7.706,57.778c0.459,3.436,2.1,6.473,4.448,8.709 C145.721,234.155,137.185,234.151,128.761,235.229z"></path>
+                                                <path style="fill:#f43f5e;" d="M169.533,60.943c0-15.511-12.614-28.125-28.125-28.125s-28.125,12.614-28.125,28.125 s12.614,28.125,28.125,28.125S169.533,76.454,169.533,60.943z M141.408,79.693c-10.341,0-18.75-8.409-18.75-18.75 s8.409-18.75,18.75-18.75s18.75,8.409,18.75,18.75S151.749,79.693,141.408,79.693z"></path>
+                                                <polygon style="fill:#f43f5e;" points="253.908,126.568 244.533,126.568 244.533,135.943 235.158,135.943 235.158,145.318 244.533,145.318 244.533,154.693 253.908,154.693 253.908,145.318 263.283,145.318 263.283,135.943 253.908,135.943 "></polygon>
+                                                <polygon style="fill:#f43f5e;" points="216.408,173.443 216.408,168.755 221.096,168.755 221.096,159.38 216.408,159.38 216.408,154.693 207.033,154.693 207.033,159.38 202.346,159.38 202.346,168.755 207.033,168.755 207.033,173.443 "></polygon>
+                                                <polygon style="fill:#f43f5e;" points="244.533,168.755 235.158,168.755 235.158,178.13 225.783,178.13 225.783,187.505 235.158,187.505 235.158,196.88 244.533,196.88 244.533,187.505 253.908,187.505 253.908,178.13 244.533,178.13 "></polygon>
+                                                <polygon style="fill:#f43f5e;" points="71.095,42.193 80.47,42.193 80.47,32.818 89.845,32.818 89.845,23.443 80.47,23.443 80.47,14.068 71.095,14.068 71.095,23.443 61.72,23.443 61.72,32.818 71.095,32.818 "></polygon>
+                                                <polygon style="fill:#f43f5e;" points="33.595,28.13 33.595,23.443 38.283,23.443 38.283,14.068 33.595,14.068 33.595,9.38 24.22,9.38 24.22,14.068 19.533,14.068 19.533,23.443 24.22,23.443 24.22,28.13 "></polygon>
+                                                <polygon style="fill:#f43f5e;" points="47.658,70.318 47.658,60.943 57.033,60.943 57.033,51.568 47.658,51.568 47.658,42.193 38.283,42.193 38.283,51.568 28.908,51.568 28.908,60.943 38.283,60.943 38.283,70.318 "></polygon>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                                <span><strong class="font-semibold text-gray-900">Ispirazione.</strong> Mi ispiro a materiali tessili di alta qualità, come l'ecopelle, la vliseline Freundenberg e il filato Madeira, che mi permettono di creare capi durevoli e di qualità superiore.</span>
+                            </li>
+                        </ul>
+                        <p class="mt-8">Il mio pezzo più rappresentativo sono i ricami, frutto del mio impegno e dedizione. I miei progetti futuri includono la creazione di borse e zaini per soddisfare la mia voglia di vedere le mie creazioni apprezzate da altre persone.</p>
+                        <h2 class="py-5 text-2xl font-bold tracking-tight text-gray-900">Il mio vero obiettivo?</h2>
+                        <p class="mt-6">Il mio obiettivo è di vedere le mie creazioni indossate da molte persone e di fare della mia passione per la moda un successo. Cucire mi dà gioia e mi consente di esprimere la mia creatività, e sono entusiasta di condividere questa esperienza con gli altri.</p>
+                    </div>
                 </div>
             </div>
-        </header>
+        </div>
     </div>
+
     <?= do_shortcode("[views section=footer name=footerView]"); ?>
 </div>
